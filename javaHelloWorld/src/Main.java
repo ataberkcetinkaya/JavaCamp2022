74b8731c9881a4eebc0d0aa68771813c5f385d57
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-09-21 11:20:17 +0300

    Video 5 & 6

---
+++ javaHelloWorld/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Merhaba JAVA");
+        System.out.println("Merhaba JAVA 2");
+    }
+}
---
+++ variables/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 14;
+        String mesaj = "Öğrenci Sayısı: ";
+        System.out.println(mesaj  + ogrenciSayisi);
+        System.out.println("Öğrenci Sayım: "  + ogrenciSayisi);
+    }
+}
