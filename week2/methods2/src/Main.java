c3c4521f2c421395c7ff19dbbc3720629165804a
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-09-22 23:19:59 +0300

    week2 - methods

---
+++ week2/methods2/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "Something";
+        String newMessage = city();
+        System.out.println(newMessage);
+
+        System.out.println(sum());
+        System.out.println(secondSum(5,7));
+    }
+
+    public static void add() { //void >> do something and stop, returns nothing!!!
+        System.out.println("Added");
+    }
+
+    public static void delete() {
+        System.out.println("Deleted");
+    }
+
+    public static void update() {
+        System.out.println("Updated");
+    }
+
+
+    public static int sum() { //int instead of void means that will return us an integer value
+        return 5;
+    }
+    public static int secondSum(int num1, int num2) { //int instead of void means that will return us an integer value
+        return num1 + num2;
+    }
+    public static String city() { //String instead of void means that will return us a String value
+        return "Brussels";
+    }
+}
