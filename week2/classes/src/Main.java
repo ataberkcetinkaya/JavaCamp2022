c1e3f2711b6d30b78bd97122578fda8105c20f1a
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-09-23 01:15:30 +0300

    value type

--- week2/classes/src/Main.java
+++ week2/classes/src/Main.java
@@ -9,5 +9,20 @@ public class Main {
 
         customerManager.Add();
         customerManager.Remove();
+
+        //value type (primitives; such as byte, long, int etc. (as variables))
+        int num1 = 10;
+        int num2 = 20;
+        num2 = num1; //num 2 becomes 10 here bc of getting the num 1 value
+        num1 = 30; //num 1 becomes 30 here but not affects the num2
+        System.out.println(num2); //so its equals to 10
+
+        //////////////////////
+        //ARRAYS are reference type like classes!!
+        int[] numbers1 = {1,2,3};
+        int[] numbers2 = {4,5,6};
+        numbers2 = numbers1;
+        numbers1[0] = 10;
+        System.out.println(numbers2[0]);
     }
 }
